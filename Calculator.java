98287a4009a191425659678d7653b96bb6a33044
Toma123456667 <suhatabassum66@gmail.com>
2022-01-12 19:59:50 +0600

    pattern change for maintain singleton pattern

--- Calculator.java
+++ Calculator.java
@@ -1,25 +1,98 @@
 public final class Calculator {
 
-    private static Calculator calculator = null;
-
-    private Calculator() {
-    }
-
-    public static Calculator getInstance() {
-        synchronized (Calculator.class) {
-            if (calculator == null) {
-                calculator = new Calculator();
-            }
-            return calculator;
-        }
-    }
-
-    public int add(int a, int b) {
-        return a + b;
-    }
+public class Calculator implements CalculationStrategy {
 
     @Override
-    protected Object clone() throws CloneNotSupportedException {
-        throw new CloneNotSupportedException();
+    public int calculate(int value1, int value2) {
+        return value1 + value2;
     }
 }
+
+
+package oopdesign.calculator;
+
+public interface CalculationStrategy {
+
+    int calculate(int value1, int value2);
+}
+
+
+package oopdesign.calculator;
+
+public class Calculator {
+
+    public static Calculator instance = null;
+
+    CalculationStrategy calculationStrategy;
+
+    public void setCalculationStrategy(CalculationStrategy calculationStrategy) {
+        this.calculationStrategy = calculationStrategy;
+    }
+
+    public static Calculator getInstance(){
+        if(instance == null){
+            instance = new Calculator();
+        }
+        return instance;
+    }
+
+    public int calculate(int value1, int value2) {
+       return calculationStrategy.calculate(value1, value2);
+    }
+}
+
+
+package oopdesign.calculator;
+
+public class CalculatorMain {
+
+    public static void main(String[] args) {
+
+        Calculator c = Calculator.getInstance();
+
+        c.setCalculationStrategy(new AdditionStrategy());
+        System.out.println(c.calculate(5 ,2));
+
+        c.setCalculationStrategy(new SubtractionStrategy());
+        System.out.println(c.calculate(5 ,2));
+
+        c.setCalculationStrategy(new MultiplicationStrategy());
+        System.out.println(c.calculate(5 ,2));
+
+        c.setCalculationStrategy(new DivideStrategy());
+        System.out.println(c.calculate(5 ,2));
+    }
+}
+
+
+package oopdesign.calculator;
+
+public class DivideStrategy implements CalculationStrategy {
+
+    @Override
+    public int calculate(int value1, int value2) {
+        return value1 / value2;
+    }
+}
+
+
+package oopdesign.calculator;
+
+public class MultiplicationStrategy implements  CalculationStrategy{
+
+    @Override
+    public int calculate(int value1, int value2) {
+        return value1 * value2;
+    }
+}
+
+package oopdesign.calculator;
+
+public class SubtractionStrategy implements CalculationStrategy {
+
+    @Override
+    public int calculate(int value1, int value2) {
+        return value1 - value2;
+    }
+}
+   
